db0e8dc6202ba7aad70a2f1bd65f0ba44247bd79
ColinKnecht <cknecht87@gmail.com>
2017-06-23 12:28:22 -0400

    commit 2

--- app/src/main/java/com/colinknecht/recyclerviewtutorial/MyRecyclerAdapter.java
+++ app/src/main/java/com/colinknecht/recyclerviewtutorial/MyRecyclerAdapter.java
@@ -1,7 +1,12 @@
 package com.colinknecht.recyclerviewtutorial;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
 
 /**
  * Created by colinknecht on 6/23/17.
@@ -9,8 +14,28 @@ import android.view.ViewGroup;
 
 public class MyRecyclerAdapter extends RecyclerView.Adapter<ListViewRowHolder> {
 
+    private List<ListItems> mListItemsList;
+    private Context mContext;
+    //ImageLoader from volley.jar?
+    private int focusedItem = 0;
+
+    public MyRecyclerAdapter(Context context, List<ListItems> itemsList) {
+        this.mListItemsList = itemsList;
+        this.mContext = context;
+    }
+
+
     @Override
-    public ListViewRowHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ListViewRowHolder onCreateViewHolder(ViewGroup viewGroup, int position) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_row,null);
+        ListViewRowHolder rowHolder = new ListViewRowHolder(v);
+
+        rowHolder.mRelativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
         return null;
     }
 
